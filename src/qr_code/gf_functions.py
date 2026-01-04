a7be3cae1b8ed6f8da81ccaec5a8ce15ff00fa9e
jonc1412 <jon.c2436@gmail.com>
2026-01-04 23:51:41 +0900

    Starting GF functions over with multiply by 2 function

--- src/qr_code/gf_functions.py
+++ src/qr_code/gf_functions.py
@@ -1,36 +1,8 @@
-# Most common irreducible polynomial of degree 8 (commonly used for QR codes)
-irreducible_poly = 0x11D
+def xtime(a : int) -> int:
+    top_bit = (a >> 7) & 0x1
+    new_bits = (a << 1) & 0xFF
 
-# Addition and subtraction work the same way in Galois Field arithmetic
-def add_sub(a, b):
-    # Negative numbers have the same value as positive numbers in Galois Field
-    a, b = abs(a), abs(b)
-
-    return a ^ b
-
-exp_table = [0] * 255
-# Important to not access log_table[0] as there is no such value a^i == 0 for some integer i
-log_table = [-1] * 256
-
-x = 1
-for i in range(255):
-    exp_table[i] = x
-    # log_table is basically the inverse of exp_table
-    log_table[x] = i
-    # Shifting to get the next power of 2
-    x <<= 1
-    if x & 0x100:
-        # Need to do polynomial modulo (not integer modulo)
-        x ^= 0x11D
-
-def mult(a, b):
-    if a == 0 or b == 0:
-        return 0
-    else:
-        return exp_table[(log_table[a] + log_table[b]) % 255]
+    if top_bit == 1:
+        new_bits = new_bits ^ 0x1D
     
-def div(a, b):
-    if a == 0 or b == 0:
-        raise ZeroDivisionError()
-    else:
-        return exp_table[(log_table[a] - log_table[b]) % 255]
+    return new_bits
--- tests/test_gf_functions.py
+++ tests/test_gf_functions.py
@@ -1,19 +1,11 @@
-import galois
-from qr_code.gf_functions import add_sub, mult, div
+import pytest
+from qr_code.gf_functions import xtime
 
-GF256 = galois.GF(2**8)
+@pytest.mark.parametrize("input, expected", [
+    (0x01, 0x02),
+    (0x80, 0x1D),
+    (0x8E, 0x01)
+])
 
-a = 0x56
-b = 0x9D
-
-GF_a = GF256(0x56)
-GF_b = GF256(0x9D)
-
-def test_add_sub():
-    assert add_sub(a, b) == GF_a + GF_b
-
-def test_mult():
-    assert mult(a, b) == GF_a * GF_b
-
-def test_div():
-    assert div(a, b) == GF_a / GF_b
+def test_xtime(input, expected):
+    assert xtime(input) == expected
